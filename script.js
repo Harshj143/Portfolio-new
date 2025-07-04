bed4f4567915dd09670c34d089220263d92fb3d8
Harshj143 <hjannawar@gmail.com>
2025-07-04 14:33:37 -0700

    front page edit

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const phrases = [
             "an AI enthusiast",
             "a Masters Student",
-            "a software engineer"
+            "a Cybersecurity engineer"
         ];
         let currentPhraseIndex = 0;
         let currentCharIndex = 0;
